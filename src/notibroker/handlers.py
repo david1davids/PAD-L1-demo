35489dc2bb79cb3f3a1ad2947a7fd4c0aa1252df
david <davvid.davids@gmail.com>
2017-10-19 21:35:37 +0300

    added message delete after dispatch

--- src/notibroker/handlers.py
+++ src/notibroker/handlers.py
@@ -25,7 +25,10 @@ def handle_command(command, payload,queue):
         LOGGER.error('Got invalid command %s', command)
         raise ValueError('Invalid command. Should be one of %s' % (COMMANDS,))
     if command == COMMANDS.send:
-        yield from _QUEUES[queue].put(payload)
+        if queue == "":
+            yield from _QUEUES["default"].put_nowait(payload)
+        else:
+            yield from _QUEUES[queue].put(payload)
         msg = 'OK'
     elif command == COMMANDS.read:
         if queue in _QUEUES:
@@ -46,21 +49,22 @@ def dispatch_message(message):
     message_type = message.get('type')
     command = message.get('command')
     queue = message.get('queue')
-    print(message)
+    # print(message)
     if message_type != MESSAGE_TYPES.command:
         LOGGER.error('Got invalid message type %s', message_type)
         raise ValueError('Invalid message type. Should be %s' % (MESSAGE_TYPES.command,))
     if command == COMMANDS.send:
         if queue not in _QUEUES:
             _QUEUES[queue] = asyncio.Queue(loop=asyncio.get_event_loop())
+
     LOGGER.debug('Dispatching command %s', command)
     response = yield from handle_command(command, message.get('payload'),queue)
     return response
 
+
 @asyncio.coroutine
 def write_queue(message):
-    #Writing persistent queue to disk
-
+    # Writing persistent queue to disk
     queue = message.get('queue')
     save_to = os.path.join(SAVE_DIRECTORY + queue)
     f = yield from aiofiles.open(save_to, mode='a')
@@ -69,8 +73,9 @@ def write_queue(message):
     finally:
         yield from f.close()
 
+
 def read_queue():
-    #Reading persistent queue from disk
+    # Reading persistent queue from disk
     directory = os.listdir(SAVE_DIRECTORY)
     if directory:
         for file in directory:
@@ -80,5 +85,14 @@ def read_queue():
                 _QUEUES[file].put_nowait(l['payload'])
             os.remove(SAVE_DIRECTORY + file)
 
-    print(_QUEUES)
+@asyncio.coroutine
+def delete_mesaage(queue):
+    f = yield from aiofiles.open(queue,mode='w+')
+    read_file = open(queue)
+    lines = read_file.readlines()
+    try:
+        yield from f.writelines([item for item in lines[:-1]])
+    finally:
+        yield from f.close()
+
 
